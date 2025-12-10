2f81f0a91fc58e4b43792076d26dc9663cd8f2a4
veronikaplenta <veronika@plenta.io>
2025-12-10 15:29:03 +0100

    ESLint

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -1,15 +1,15 @@
-import "../scss/backend.scss";
+import '../scss/backend.scss';
 
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
-export const app = startStimulusApp(require.context(
+export default startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
-    /\.[jt]sx?$/
+    /\.[jt]sx?$/,
 ));
 
-/*document.addEventListener('DOMContentLoaded', function () {
+/* document.addEventListener('DOMContentLoaded', function () {
     const loadingClass = 'loading';
 
     document.querySelectorAll('.loki-prompt-button button').forEach(function (item) {
@@ -96,4 +96,4 @@ document.addEventListener('DOMContentLoaded', async function () {
             }
         }
     }
-})*/
+}) */
