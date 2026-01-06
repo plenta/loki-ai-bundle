f9a03b97e6dbad19588ca790029120335653cb79
veronikaplenta <veronika@plenta.io>
2026-01-06 13:16:28 +0100

    Merge code-quality

--- src/LokiAiBundle.php
+++ src/LokiAiBundle.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * Loki AI Bundle for Contao Open Source CMS
  *
- * @copyright     Copyright (c) 2025, Plenta.io
+ * @copyright     Copyright (c) 2026, Plenta.io
  * @author        Plenta.io <https://plenta.io>
  * @link          https://github.com/plenta/
  */
